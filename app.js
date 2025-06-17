bf1cdbb42af15e397304567e4cee449f77710754
DESKTOP-29VI7GO\user <ssssssy0@naver.com>
2025-06-17 18:10:05 +0900

    no message

--- app.js
+++ app.js
@@ -2,16 +2,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { createClient } = require('@supabase/supabase-js');
+const multer = require('multer');
+const session = require('express-session');
 
 require("dotenv").config();  // .env
 const cors = require('cors');
 const nunjucks = require("nunjucks");
 
-const indexRouter = require('./routes/index');
-const signupRouter = require('./routes/signup');
-const homeRouter = require('./routes/home');
 
-var app = express();
+const app = express();
+
+// Supabase 
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+
+// Multer 설정 
+const upload = multer({ storage: multer.memoryStorage() });
+
+// // 세션 설정
+// app.use(session({
+//   secret: process.env.SESSION_SECRET || 'env시크릿키추가하기',
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: { secure: false }, // 배포 시 true로 설정하기 !! 
+// }));
 
 app.use(cors())
 app.use(logger('dev'));
@@ -20,14 +34,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// nunjucks 설정
 app.set("view engine", "html");
 nunjucks.configure("views", {
   express: app,
   watch: true,
 });
 
+// Supabase 미들웨어 
+app.use((req, res, next) => {
+  req.supabase = supabase;
+  next();
+});
+
+
+
+// 라우터
+
+const indexRouter = require('./routes/index');
+const signupRouter = require('./routes/signup');
+const homeRouter = require('./routes/home');
+
 app.use('/', indexRouter);
-app.use('/signup', signupRouter);
+app.use('/signup',upload.single('image_url'), signupRouter);
 app.use('/home', homeRouter);
 
 module.exports = app;
--- routes/signup.js
+++ routes/signup.js
@@ -1,8 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('signup', { title: '회원가입' });
+  res.render('signup', { title: '회원가입' });
+});
+
+router.post('/', async function(req, res, next) {
+  const { name, phone, email, password, postcode, address, address_detail, company_no} = req.body;
+
+  const supabase = req.supabase;
+  console.log(req.body);
+
+  const addr = [postcode, address, address_detail].filter(Boolean).join(', ');
+
+
+  const { data, error } = await supabase.from('customer').insert({
+    name,
+    phone,
+    email,
+    password,
+    addr,
+    company_no,
   });
+  
+});
 
 module.exports = router;
